9f07aa56bc5b17c2e0734b70d49b62d7278cd10b
HG <hg@HGui-MacBookAir.local>
2022-11-29 14:46:06 +0900

    first commit

---
+++ js/about.dfdbb145.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcoinweb"]=self["webpackChunkcoinweb"]||[]).push([[443],{7381:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(n,e){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=u(89);const o={},b=(0,i.Z)(o,[["render",s]]);var f=b}}]);
+//# sourceMappingURL=about.dfdbb145.js.map
